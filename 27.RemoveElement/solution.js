e0cd689f675ee6bbfbc459e90a0d2d4c9b3dc2f8
Ben Daniels <benjamindaniels21@gmail.com>
2022-01-26 20:24:53 -0500

    add 66. Plus One

--- 27.RemoveElement/solution.js
+++ 27.RemoveElement/solution.js
@@ -5,3 +5,5 @@
 // Return k after placing the final result in the first k slots of nums.
 
 // Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+
